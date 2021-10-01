0a0c84ceb9c054cb15366508c86d09651d5140f1
Irvin Szeto <irvins@stanford.edu>
2021-10-01 10:44:53 -0700

    addign getSafePath

--- endpoints/passthrough.php
+++ endpoints/passthrough.php
@@ -2,15 +2,15 @@
 namespace Stanford\AIMI;
 /** @var \Stanford\AIMI\AIMI $module */
 
-$em_setting = (isset($_GET["em_setting"])) ? filter_var($_GET["em_setting"], FILTER_SANITIZE_STRING) : null;
-$file_name  = (isset($_GET["shard"])) ? filter_var($_GET["shard"], FILTER_SANITIZE_STRING) : null;
+$em_setting     = (isset($_GET["em_setting"])) ? filter_var($_GET["em_setting"], FILTER_SANITIZE_STRING) : null;
+$shard_name     = (isset($_GET["shard"])) ? filter_var($_GET["shard"], FILTER_SANITIZE_STRING) : null;
 
 $active_alias   = $module->getProjectSetting("active_alias");
 $aliases        = $module->getProjectSetting('aliases');
 $current        = $aliases[$active_alias];
 $shard_paths    = array_key_exists("model_json",$current) && !empty($current["model_json"]) ? $current["model_json"]["weightsManifest"][0]["paths"] : array();
 
-if($em_setting && !$file_name){
+if($em_setting && !$shard_name){
     if($em_setting == "config_js"){
         header("content-type: application/javascript");
         echo $current["config_js"];
@@ -20,12 +20,12 @@ if($em_setting && !$file_name){
         header("content-type: application/json");
         echo json_encode($current["model_json"]);
     }
-}elseif($file_name){
+}elseif($shard_name){
     // Make sure the requested file is one of the desired active model's shard paths
     $model_file_check = null;
     foreach($shard_paths as $shard_path ){
-        if(strpos($shard_path , $file_name) > -1){
-            $model_file_check = APP_PATH_TEMP . $file_name;
+        if(strpos($shard_path , $shard_name) > -1){
+            $model_file_check = $module->getSafePath($shard_name, APP_PATH_TEMP);
             break;
         }
     }
